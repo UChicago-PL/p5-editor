e829dfcd3461405a033d48a92d08d86b56db71be
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2021-03-08 18:05:13 -0600

    Fix first run bug

--- client/modules/IDE/actions/ide.js
+++ client/modules/IDE/actions/ide.js
@@ -265,7 +265,25 @@ export function startRefreshSketch() {
 }
 
 export function startSketch() {
+  // if (!window.runOnce) {
+  //   return (dispatch) => {
+
+  //     console.log('here!!');
+  //     dispatch(clearConsole());
+  //     dispatch(startVisualSketch());
+  //     dispatch(startRefreshSketch());
+  //     dispatch(logRun('run-manual'));
+  //     window.runOnce = true;
+  //   };
+  // }
   return checkLoggedIn((dispatch) => {
+    if (!window.runOnce) {
+      setTimeout(() => {
+        console.log('here?');
+        dispatch(startVisualSketch());
+      }, 1000);
+      window.runOnce = true;
+    }
     dispatch(clearConsole());
     dispatch(startVisualSketch());
     dispatch(startRefreshSketch());
