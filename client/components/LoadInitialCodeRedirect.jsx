cea2f969e0c8b285bacbb605a4eac0541e83284f
Andrew McNutt <mcnutt.andrew@gmail.com>
2021-03-17 14:26:23 -0500

    Receive named file objects (#76)
    * Recieve named file objects
    
    * clean up

--- client/components/LoadInitialCodeRedirect.jsx
+++ client/components/LoadInitialCodeRedirect.jsx
@@ -3,17 +3,52 @@ import PropTypes from 'prop-types';
 import { JSONUncrush } from 'jsoncrush';
 import { browserHistory } from 'react-router';
 import { updateFileContent } from '../modules/IDE/actions/files';
+import { setProjectName } from '../modules/IDE/actions/project';
 
 function LoadInitialCodeRedirect({ store, code }) {
   if (code) {
     useEffect(() => {
       const decodedCode = JSONUncrush(code);
-      const scriptFile = store.getState().files.find((file) => file.name === 'sketch.js');
+      let parsedCode;
+      try {
+        parsedCode = JSON.parse(decodedCode);
+      } catch (e) {
+        console.log('error', e);
+        parsedCode = decodedCode;
+      }
+      if (typeof parsedCode === 'string') {
+        const scriptFile = store.getState().files.find((file) => file.name === 'sketch.js');
 
-      // cool gotcha: the initial state for the project doesn't include any files, so trying to
-      // run saveProject with specific code (and thus setting `fileSelected` to not equal null) throws
-      // this does not work: saveProject(decodedCode)
-      store.dispatch(updateFileContent(scriptFile.id, decodedCode));
+        // cool gotcha: the initial state for the project doesn't include any files, so trying to
+        // run saveProject with specific code (and thus setting `fileSelected` to not equal null) throws
+        // this does not work: saveProject(parsedCode)
+        store.dispatch(updateFileContent(scriptFile.id, parsedCode));
+      }
+      if (typeof parsedCode === 'object') {
+        // find all of the files with names like we expect, replace em
+        // otherwise createFile at root
+        parsedCode.files.forEach((newFile) => {
+          const oldFile = store.getState().files.find((file) => file.name === newFile.name);
+          if (oldFile) {
+            console.log('overwrote');
+            store.dispatch(updateFileContent(oldFile.id, newFile.content));
+          } else {
+            // WIP - for now we can only import files with the standard naming convention
+            // const rootId = store.getState().files.find((file) => file.name === 'root').id;
+            // const preppedFile = {
+            //   children: [],
+            //   fileType: 'file',
+            //   isSelectedFile: false,
+            //   name: newFile.name,
+            //   content: newFile.content,
+            //   parentId: rootId
+            // };
+            // store.dispatch(createFile(preppedFile, rootId));
+          }
+        });
+        store.dispatch(setProjectName(parsedCode.projectName));
+      }
+
       browserHistory.replace('/');
     });
   } else {
