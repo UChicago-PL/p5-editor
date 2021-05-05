6a708823502db780f738cf1292ddb565164664dd
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2021-05-05 15:27:12 -0500

    tweak

--- server/migrations/start.js
+++ server/migrations/start.js
@@ -2,7 +2,7 @@ require('@babel/register');
 require('@babel/polyfill');
 const path = require('path');
 require('dotenv').config({ path: path.resolve('.env.production') });
-require('./emailConsolidation');
+// require('./emailConsolidation');
 // require('./populateTotalSize');
 // require('./moveBucket');
 // require('./truncate');
