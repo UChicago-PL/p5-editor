a9bd1b37902a310b1536d18bbc98443eebb6b38a
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2021-06-23 14:23:04 -0500

    default to creating everyone

--- server/models/userAllowlist.js
+++ server/models/userAllowlist.js
@@ -20,17 +20,17 @@ userAllowlistSchema.index({ github: 1, edition: 1 }, { unique: true });
 
 const UserAllowlist = mongoose.model('UserAllowlist', userAllowlistSchema);
 
-if (process.env.NODE_ENV === 'development') {
-  const users = ['mcnuttandrew', 'mcnuttandrew-test', 'brianhempel', 'ravichugh'];
+// if (process.env.NODE_ENV === 'development') {
+const users = ['mcnuttandrew', 'mcnuttandrew-test', 'brianhempel', 'ravichugh'];
 
-  users.forEach((user) => {
-    const cb = (e) => {
-      // there is a uniqueness constraint on usernames, so no duplicates will be created
-      // ignore the uniqueness error
-    };
-    UserAllowlist.create({ github: user, type: 'user', studyParticipant: false, edition: 'csp21' }, cb);
-    UserAllowlist.create({ github: user, type: 'user', studyParticipant: false, edition: 'imm21' }, cb);
-  });
-}
+users.forEach((user) => {
+  const cb = (e) => {
+    // there is a uniqueness constraint on usernames, so no duplicates will be created
+    // ignore the uniqueness error
+  };
+  UserAllowlist.create({ github: user, type: 'user', studyParticipant: true, edition: 'csp21' }, cb);
+  UserAllowlist.create({ github: user, type: 'user', studyParticipant: true, edition: 'imm21' }, cb);
+});
+// }
 
 export default UserAllowlist;
