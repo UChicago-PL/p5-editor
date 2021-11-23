4c3c123f15e421217f95c45e1251e53a411c388c
Anton <antonoutkine@gmail.com>
2021-11-23 12:57:22 -0600

    ShapeToolbox: Add circle function, handle rotation, and round numbers

--- client/modules/IDE/components/ShapeToolboxOverlay.jsx
+++ client/modules/IDE/components/ShapeToolboxOverlay.jsx
@@ -26,6 +26,64 @@ export default function ShapeToolbox({ closeCb, canvasSize }) {
     );
   };
 
+  const addCircle = () => {
+    canvas.add(
+      new fabric.Circle({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        radius: 10
+      })
+    );
+  };
+
+  const generateFuncCalls = (o) => {
+    const func = (() => {
+      switch (o.type) {
+        case 'rect':
+          return ['rect', o.left, o.top, o.width * o.scaleX, o.height * o.scaleY];
+        case 'circle':
+          if (o.scaleX === o.scaleY) {
+            return [
+              'circle',
+              o.left + o.radius * o.scaleX,
+              o.top + o.radius * o.scaleX,
+              o.radius * 2 * o.scaleX
+            ];
+          }
+          return [
+            'ellipse',
+            o.left + (o.width * o.scaleX) / 2,
+            o.top + (o.height * o.scaleY) / 2,
+            o.width * o.scaleX,
+            o.height * o.scaleY
+          ];
+        default:
+          return null;
+      }
+    })();
+    if (!func) return [];
+    if (o.angle) {
+      return [
+        ['translate', func[1], func[2]],
+        ['rotate', `radians(${o.angle})`],
+        [func[0], 0, 0, ...func.slice(3)],
+        ['rotate', `radians(${-o.angle})`],
+        ['translate', -func[1], -func[2]]
+      ];
+    }
+    return [func];
+  };
+
+  const roundNums = (xs) => {
+    return xs.map((x) => {
+      if (typeof x === 'number') {
+        return Math.round(x * 100) / 100;
+      }
+      return x;
+    });
+  };
+
   const generateFuncString = ([name, ...args]) => {
     return `${name}(${args.join(',')})`;
   };
@@ -33,17 +91,7 @@ export default function ShapeToolbox({ closeCb, canvasSize }) {
   const apply = () => {
     const data = canvas.toObject();
     console.log(data);
-    const lines = data.objects
-      .map((o) => {
-        switch (o.type) {
-          case 'rect':
-            return ['rect', o.left, o.top, o.width * o.scaleX, o.height * o.scaleY];
-          default:
-            return null;
-        }
-      })
-      .filter(Boolean)
-      .map(generateFuncString);
+    const lines = data.objects.flatMap(generateFuncCalls).map(roundNums).map(generateFuncString);
     canvas.clear();
     closeCb(lines);
   };
@@ -52,6 +100,7 @@ export default function ShapeToolbox({ closeCb, canvasSize }) {
     <div className="shape-toolbox-overlay">
       <canvas ref={el} />
       <button onClick={addRect}>rect</button>
+      <button onClick={addCircle}>circle</button>
       <button onClick={apply}>apply</button>
     </div>
   );
