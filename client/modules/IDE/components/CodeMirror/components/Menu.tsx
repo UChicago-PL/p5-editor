fe53ca2f47001747f7b036a344c67192fab98ba8
Andrew McNutt <mcnutt.andrew@gmail.com>
2021-12-10 15:14:29 -0600

    swap menu order (#140)

--- client/modules/IDE/components/CodeMirror/components/Menu.tsx
+++ client/modules/IDE/components/CodeMirror/components/Menu.tsx
@@ -1,23 +1,21 @@
-import * as React from "react"
-import { Dispatch, State } from "../state/state"
+import * as React from 'react';
+import { Dispatch, State } from '../state/state';
 
 type Props = {
-  dispatch: Dispatch
-  state: State
-}
+  dispatch: Dispatch;
+  state: State;
+};
 
 export default function Menu({ dispatch, state }: Props) {
   return (
     <div className="codemirror__menu">
-      {/*<button onClick={() => dispatch({ type: "toggleBoolWidgets" })}>*/}
-      {/*  Toggle Bool*/}
-      {/*</button>*/}
-      <button onClick={() => dispatch({ type: "toggleNumWidgets" })}>
-        {state.showNumWidgets ? "Hide" : "Show"} Number Pickers
+      <button onClick={() => dispatch({ type: 'toggleColorWidgets' })}>
+        {state.showColorWidgets ? 'Hide' : 'Show'} Color Pickers
       </button>
-      <button onClick={() => dispatch({ type: "toggleColorWidgets" })}>
-        {state.showColorWidgets ? "Hide" : "Show"} Color Pickers
+      <button onClick={() => dispatch({ type: 'toggleNumWidgets' })}>
+        {state.showNumWidgets ? 'Hide' : 'Show'} Number Pickers
       </button>
+      {/* <button onClick={() => dispatch({ type: 'toggleBoolWidgets' })}>Toggle Bool</button> */}
     </div>
-  )
+  );
 }
