8b7abc1e0e2a577be78f354a2d6c6f6c61fded60
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2021-12-13 09:45:13 -0600

    Fix safarai regex bug

--- client/modules/IDE/components/CodeMirror/state/colorRegex.ts
+++ client/modules/IDE/components/CodeMirror/state/colorRegex.ts
@@ -1,2 +1,3 @@
+/* eslint-disable max-len */
 // https://gist.github.com/olmokramer/82ccce673f86db7cda5e#gistcomment-2029233
-export default /(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))/i
+export default /(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))/i;
