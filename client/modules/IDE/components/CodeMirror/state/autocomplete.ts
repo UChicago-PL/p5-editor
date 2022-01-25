37bd87922e1fcc698ac422c12f5af7d6133b1c1d
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2022-01-25 14:41:09 -0600

    insturment autocomplete

--- client/modules/IDE/components/CodeMirror/state/autocomplete.ts
+++ client/modules/IDE/components/CodeMirror/state/autocomplete.ts
@@ -1,5 +1,6 @@
-import { autocompletion, CompletionSource } from '@codemirror/autocomplete';
-import words, { snippets } from './autocomplete-words';
+import { autocompletion, CompletionSource, snippet } from '@codemirror/autocomplete';
+import words, { preCompletions } from './autocomplete-words';
+import { trackEvent } from '../../../../../utils/analytics';
 
 function from(list: string[]): CompletionSource {
   return (cx) => {
@@ -7,9 +8,20 @@ function from(list: string[]): CompletionSource {
     if (!word && !cx.explicit) {
       return null;
     }
+    function apply(template: string) {
+      const snipBind = snippet(template);
+      return function snippetCatcher(view, completion, from, to) {
+        // console.log('here i am', completion, completion.shortLabel || completion.label);
+        trackEvent({ eventName: 'ac', context: completion.shortLabel || completion.label });
+        snipBind(view, completion, from, to);
+      };
+    }
     return {
       from: word ? word.from : cx.pos,
-      options: snippets.concat(list.map((w) => ({ label: w }))),
+      options: ([] as any)
+        .concat(preCompletions.map((comp) => ({ ...comp, apply: apply(comp.template) })))
+        .concat(list.map((label) => ({ label, apply: apply(label) }))),
+      // preCompletions
       span: /\w*/
     };
   };
