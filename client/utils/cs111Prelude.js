57542aa953baf9a44d50a60f0480cf12774234ad
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2022-01-26 12:52:47 -0600

    Inject prelude

--- client/utils/cs111Prelude.js
+++ client/utils/cs111Prelude.js
@@ -7,6 +7,17 @@ const Editor = {
 }
 `;
 
+export function injectPrelude(code) {
+  const script = `<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/UChicago-PL/cs11111-assets/prelude.js" ></script>`;
+  const splits = code.split('</head>');
+  if (splits.length !== 2) {
+    console.error('Prelude injection failed on \n', code);
+    return code;
+  }
+
+  return `${splits[0]}\n${script}\n</head>${splits[1]}`;
+}
+
 export function strip(code) {
   return code
     .replace(/Editor\.slider\(([\d\s,]+)\)/, (match, args) => parseInt(args.split(',')[2]) || 0)
